63cf9ff6d0ae0b40a469317bfef1a01dc305e287
Zunaid-jony <jony35666666@gmail.com>
2022-06-29 03:48:26 +0600

    updats

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,28 +1,28 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import data from "./../data";
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
-    useEffect(()=>{
-        const fetchData = async () =>{
-            const result = await axios.get('/api/products')
-            setProducts(result.data)
-        }
-        fetchData()
-
-
-    },[])
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("/api/products");
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
       {/* products */}
 
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
-            <Link style={{ textDecoration:'none'}} to={`/product/${product.slug}`}>
+            <Link
+              style={{ textDecoration: "none" }}
+              to={`/product/${product.slug}`}
+            >
               <img src={product.image} alt={product.name}></img>
             </Link>
             <div className="product-info">
