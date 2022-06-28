7627d02682071f9858663dd81b80e99c6472339a
Zunaid-jony <jony35666666@gmail.com>
2022-06-28 19:50:49 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-
+import data from "./data";
 
 function App() {
   return (
     <div>
       <header>
-        <a href='/'>E-commarce</a>
+        <a href="/">E-commarce</a>
       </header>
-      <main>list Product</main>
-      
+      <main>
+        <h1>Featured Products</h1>
+        {/* products */}
+
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product"
+             key={product.slug}>
+              <img src={product.image} alt={product.name}></img>
+              <p> {product.name} </p>
+              <p> $ {product.price} </p>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
