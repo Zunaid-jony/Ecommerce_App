faaacf070b69d3cedff0fae4db8cf2438d127da8
Zunaid-jony <jony35666666@gmail.com>
2022-06-29 14:24:05 +0600

    menu bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
+import { Container, Navbar } from "react-bootstrap";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import {LinkContainer} from 'react-router-bootstrap'
 
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
+    <div className="d-flex flex-column  site-container">
       <header>
-        <Link to="/">E-commarce</Link>
+        <Navbar bg='dark' variant="dark">
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>E-commarce </Navbar.Brand>
+            </LinkContainer> 
+          </Container>
+        </Navbar>
+   
       </header>
       <main>
-        <Routes>
+      <Container>
+      <Routes>
           <Route path="/" element={<HomeScreen></HomeScreen>}></Route>
           <Route path="/product/:slug" element={<ProductScreen></ProductScreen>}></Route>
         </Routes>
+      </Container>
+
         
       </main>
+      <footer>
+        <div className="text-center ">
+
+         2022 Jony all &copy; rights
+          </div> 
+      </footer>
+
     </div>
     </BrowserRouter>
   );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,23 +1,68 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
+import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import logger from "use-reducer-logger";
+
+
+const reducer = (state, action) =>{
+  switch(action.type){
+    case'FETCH_REQUEST':
+    return {...state, loading: true}
+    case'FETCH_SUCCESS':
+    return {...state, products: action.payload, loading: false}
+    case'FETCH_FAIL':
+    return {...state, loading:false, error:action.payload}
+    default:
+      return state;
+  }
+}
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+
+ 
+  const [{loading, error, products}, dispatch] = useReducer(logger(reducer),{
+    products: [],
+    loading: true,
+    error:''
+  })
+  //const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get("/api/products");
-      setProducts(result.data);
+      dispatch({type: 'FETCH_REQUEST'})
+      try{
+        const result = await axios.get("/api/products");
+        dispatch({type: 'FETCH_SUCCESS', payload: result.data})
+
+      }
+      catch(err){
+        dispatch({type: 'FETACH_FAIL', payload: err.message})
+
+      }
+     
+      //setProducts(result.data);
     };
     fetchData();
   }, []);
   return (
     <div>
       <h1>Featured Products</h1>
+      {/* <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '} */}
+      <Button variant="info">Info</Button>{' '}
+      
       {/* products */}
 
       <div className="products">
-        {products.map((product) => (
+        {
+        loading ? (<div> Loading...</div>)
+        :
+        error ? (<div> {error}</div>
+          ) : (
+        products.map((product) => (
           <div className="product" key={product.slug}>
             <Link
               style={{ textDecoration: "none" }}
@@ -33,10 +78,10 @@ const HomeScreen = () => {
                 {" "}
                 <strong> $ {product.price}</strong>{" "}
               </p>
-              <button>Add to Cart</button>
+              <Button variant="info">Add to Cart</Button>
             </div>
           </div>
-        ))}
+        )))}
       </div>
     </div>
   );
