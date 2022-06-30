22dcfd5d4fa10a8f6c902dddadb71308aaab66ac
Zunaid-jony <jony35666666@gmail.com>
2022-06-30 21:21:09 +0600

    problem solved

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
-import { axios } from "axios";
+import axios from "axios";
 import { useReducer } from "react";
 
 const reducer = (state, action) => {
@@ -28,11 +28,16 @@ const ProductScreen = () => {
 
   useEffect(() => {
     const fetchData = async () => {
+      console.log('fetching data-----------')
       dispatch({ type: "FETCH_REQUEST" });
+      // console.log('after dispatch--------')
       try {
+        console.log('inside try-------')
         const result = await axios.get(`/api/products/slug/${slug}`);
+        console.log('ddddddd', result)
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (err) {
+        console.log('err---', err)
         dispatch({ type: "FETACH_FAIL", payload: err.message });
       }
     };
@@ -44,7 +49,9 @@ const ProductScreen = () => {
   ) : error ? (
     <div>{error}</div>
   ) : (
-    <div>{product.name}</div>
+    <div>{product.name}
+    <img src={product.image}></img>
+    </div>
   );
 };
 
