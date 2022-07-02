c9cced6cb95ee09091fb28769cbcbfd558973dcc
Zunaid-jony <jony35666666@gmail.com>
2022-07-03 02:11:27 +0600

    chak out

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useContext } from 'react';
 import { Store } from './../Store';
-import { Col, ListGroup, ListGroupItem, Row } from 'react-bootstrap';
+import { Card, Col, ListGroup, ListGroupItem, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 
@@ -20,7 +20,7 @@ const CartScreen = () => {
             <h2>Shopping Cart</h2>
             <br></br>
             <Row> 
-                <Col md={10}>
+                <Col md={8}>
                     {cartItems.length === 0 ? (
                        <p>
                            Cart is empty. <Link to="/"> Go Shopping </Link>
@@ -47,7 +47,7 @@ const CartScreen = () => {
                                            </Button> {''}
 
                                            <span> {item.quantity}</span>{''}
-                                           <Button variant='light' disabled={item.quantity === 1}>
+                                           <Button variant='light' disabled={item.quantity === item.countInStock}>
                                                <i className='fas fa-plus-circle'> </i>
 
                                            </Button>
@@ -71,7 +71,33 @@ const CartScreen = () => {
                     )
                 }
                 </Col>
-                <Col md={4}></Col>
+                <Col md={4}>
+                <Card>
+            <Card.Body>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <h3>
+                    Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{' '}
+                    items) : $
+                    {cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
+                  </h3>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <div className="d-grid">
+                    <Button
+                      type="button"
+                      variant="primary"
+                      
+                      disabled={cartItems.length === 0}
+                    >
+                      Proceed to Checkout
+                    </Button>
+                  </div>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Card>
+                </Col>
                 
             </Row> 
         </div>
