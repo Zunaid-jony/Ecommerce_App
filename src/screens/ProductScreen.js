e6ffb4468209a141a2045bd31cb64a64589cb219
Zunaid-jony <jony35666666@gmail.com>
2022-07-02 23:57:26 +0600

    add products

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect,useReducer } from "react";
 import axios from "axios";
-import { useReducer } from "react";
+
 import { Badge, Button, Col, Row, Spinner } from "react-bootstrap";
 import Rating from "../components/Rating";
 import { Helmet } from "react-helmet-async";
@@ -49,25 +49,24 @@ const ProductScreen = () => {
     };
     fetchData();
   }, [slug]);
-  const {state, dispatch: ctxDispatch} = useContext(Store);
-  const {cart} = state;
 
-  const addToCartHandler = async() =>{
-    console.log('abc..')
-    const existItem = cart.cartItems.find((x) => x._id === product._id)
-    console.log('cdf')
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart} = state;
+  const addToCartHandler = async() => {
+    const existItem  = cart.cartItems.find((x) => x._id === product._id);
     const quantity = existItem ? existItem.quantity + 1 : 1;
-    const {data} = await axios.get(`/api/products/${product._id}`);
-    if(data.countInStock < quantity){
-      window.alert('Sorry. Product is out of stock')
-      return;
-    }
+    // const { data } = await axios.get(`/api/products/${product._id}`);
+    // console.log("ddddddd", data);
+    // if (data.countInStock < quantity) {
+    //   window.alert('Sorry. Product is out of stock');
+    //   return;
+    // }
     ctxDispatch({
       type: 'CART_ADD_ITEM',
-      payload: {...product, quantity: 1},
-    })
-
-  }
+      payload: { ...product, quantity },
+    });
+  
+  };
 
   return loading ? (
     <div><Spinner animation="border" /></div>
