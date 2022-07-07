17b556302108cfff35372daccbecf10a53a1d074
Zunaid-jony <jony35666666@gmail.com>
2022-07-07 23:09:54 +0600

    ddfdfddfd

--- src/screens/Route.js
+++ src/screens/Route.js
@@ -1,46 +1,11 @@
-
 import React from 'react';
-import { Container, Form } from 'react-bootstrap';
-import { Helmet } from 'react-helmet-async';
-import { Button } from 'react-bootstrap';
-import { Link, useLocation } from 'react-router-dom';
 
-const SigninScreen = () => {
-    const {search} = useLocation();
-    const redirectInUrl = new URLSearchParams(search).get('redirect')
-    const redirect = redirectInUrl ? redirectInUrl : '/';
+const Route = () => {
     return (
-       <div style={{width:'40%', marginLeft:'30%'}}>
-            <Container className='small-container'>
-            <Helmet>
-                <title>Login</title>
-            </Helmet>
-            <h1 className='my-3' style={{ textAlign: 'center'}}> Login</h1>
-            <Form>
-                <Form.Group className='mb-3' controlId='email'>
-                    <Form.Label>Email</Form.Label> 
-                    <Form.Control type='email' required></Form.Control>
-
-                </Form.Group>
-                {/* pssword */}
-                <Form.Group className='mb-3' controlId='password'>
-                    <Form.Label>Password</Form.Label> 
-                    <Form.Control type='passwoed' required></Form.Control>
-                    <div className='mb-3'>
-                        <Button type="submit"> Login.</Button>
-                    </div>
-                    <div className='mb-3'>
-                        New Customer?{''}
-                        <Link to={`/signup?redirect=${redirect}`}> Create your account</Link>
-
-                    </div>
-
-                </Form.Group>
-            </Form>
-            
-        </Container>
-       </div>
+        <div>
+            <h1>Thsi is route pages</h1>
+        </div>
     );
 };
 
-export default SigninScreen;
+export default Route;
