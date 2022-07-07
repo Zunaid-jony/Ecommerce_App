bb7ed859c065d417cf7e4bb73201637f85b3367c
Zunaid-jony <jony35666666@gmail.com>
2022-07-07 23:09:09 +0600

    aaaa

--- src/screens/Route.js
+++ src/screens/Route.js
@@ -1,11 +1,26 @@
-import React from 'react';
 
-const Route = () => {
+import React from 'react';
+import { Container, Form } from 'react-bootstrap';
+import { Helmet } from 'react-helmet-async';
+import { Button } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
+
+const SigninScreen = () => {
+    const {search} = useLocation();
+    const redirectInUrl = new URLSearchParams(search).get('redirect')
+    const redirect = redirectInUrl ? redirectInUrl : '/';
     return (
-        <div>
-            <h1>Thsi is route pages</h1>
-        </div>
+       <div style={{width:'40%', marginLeft:'30%'}}>
+            <Container className='small-container'>
+            <Helmet>
+                <title>Login</title>
+            </Helmet>
+            <h1 className='my-3' style={{ textAlign: 'center'}}> Login</h1>
+           
+            
+        </Container>
+       </div>
     );
 };
 
-export default Route;
+export default SigninScreen;
