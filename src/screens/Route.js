d7ab8e58cc177a14081a9ee5e2d8abfda062f6fb
Zunaid-jony <jony35666666@gmail.com>
2022-07-07 23:09:37 +0600

    ddfdfd

--- src/screens/Route.js
+++ src/screens/Route.js
@@ -16,7 +16,27 @@ const SigninScreen = () => {
                 <title>Login</title>
             </Helmet>
             <h1 className='my-3' style={{ textAlign: 'center'}}> Login</h1>
-           
+            <Form>
+                <Form.Group className='mb-3' controlId='email'>
+                    <Form.Label>Email</Form.Label> 
+                    <Form.Control type='email' required></Form.Control>
+
+                </Form.Group>
+                {/* pssword */}
+                <Form.Group className='mb-3' controlId='password'>
+                    <Form.Label>Password</Form.Label> 
+                    <Form.Control type='passwoed' required></Form.Control>
+                    <div className='mb-3'>
+                        <Button type="submit"> Login.</Button>
+                    </div>
+                    <div className='mb-3'>
+                        New Customer?{''}
+                        <Link to={`/signup?redirect=${redirect}`}> Create your account</Link>
+
+                    </div>
+
+                </Form.Group>
+            </Form>
             
         </Container>
        </div>
