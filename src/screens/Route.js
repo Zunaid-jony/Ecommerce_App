c3a7f045ac6d789178c2af4013414840841386f8
Zunaid-jony <jony35666666@gmail.com>
2022-07-15 22:40:26 +0600

    dfsdfdfddfs

--- src/screens/Route.js
+++ src/screens/Route.js
@@ -4,15 +4,6 @@ const Route = () => {
     return (
         <div>
             <h1>Thsi is route pages</h1>
-            <h1>Thsi is route pages</h1>
-            <h1>Thsi is route pages</h1>
-            <h1>Thsi is route pages</h1>
-            <h1>Thsi is route pages</h1>
-            <h1>Thsi is route pages</h1>
-            <h1>Thsi is route pages</h1>
-            <h1>Thsi is route pages</h1>
-            <h1>Thsi is route pages</h1>
-            <h1>Thsi is route pages</h1>
         </div>
     );
 };
